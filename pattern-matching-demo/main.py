9dbf1954ed73f8ac7618a1134a7ace233695e07e
Bharathy Poovalingam <bharathy.poovalingam@gmail.com>
2025-05-13 11:05:07 +1000

    util to match email pattern and extract email id out of input string

--- pattern-matching-demo/main.py
+++ pattern-matching-demo/main.py
@@ -1,12 +1,13 @@
 import re
-from pattern_match_util import get_phone_no
+from pattern_match_util import get_phone_no,get_email_id
 def main():
     print("Hello from pattern-matching-demo!")
     msg_phone1 = ' My phone no is 1234567891 and 9876543211'
     msg_phone2 = ' My phone no is (123)-456-7891 and 9876543211'
+    msg_email = ' My email id is sachin.bharathy@gmail.com'
     print(get_phone_no(msg_phone1))
     print(get_phone_no(msg_phone2))
-
+    print(get_email_id(msg_email))
 
 if __name__ == "__main__":
     main()
--- pattern-matching-demo/pattern_match_util.py
+++ pattern-matching-demo/pattern_match_util.py
@@ -1,5 +1,16 @@
 import re
+
+''' function to match the phone no from 
+    the input string and return it '''
 def get_phone_no(input_data):
     pattern = r'\d{10}|\(\d{3}\)-\d{3}-\d{4}'
     phone_no = re.findall(pattern,input_data)
     return phone_no
+
+
+''' function to match the email id from 
+    the input string and return it '''
+def get_email_id(input_data):
+    pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b'
+    email_id = re.findall(pattern,input_data)
+    return email_id
